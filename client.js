cac75e62ae82920b2070cf6a4d42d09925e58aef
Haisheng W - M <haisheng@mercury.com>
2023-09-02 08:55:02 -0700

    adjust query

--- client.js
+++ client.js
@@ -10,20 +10,17 @@ const pgclient = new Client({
 
 pgclient.connect();
 
-const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
-
-pgclient.query(table, (err, res) => {
+pgclient.query('select version();', (err, res) => {
     if (err) throw err
+    console.log(err, res.rows);
 });
-
-pgclient.query(text, values, (err, res) => {
+pgclient.query("select '_' collate \"en_US\" < 'e' collate \"en_US\";", (err, res) => {
     if (err) throw err
+    console.log(err, res.rows);
 });
-
-pgclient.query('SELECT * FROM student', (err, res) => {
+pgclient.query("select 'lock_note' collate \"en_US\" < 'locked_by' collate \"en_US\";", (err, res) => {
     if (err) throw err
-    console.log(err, res.rows) // Print the data in student table
+    console.log(err, res.rows);
     pgclient.end()
 });
+
