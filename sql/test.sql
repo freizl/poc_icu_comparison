a0af10ac54a2cfee7eb8b67fb318acdd192c1769
Haisheng W - M <haisheng@mercury.com>
2023-09-02 21:44:36 -0700

    tries other queries

--- sql/test.sql
+++ sql/test.sql
@@ -1,3 +1,16 @@
-select version();
-select '_' collate "en_US" < 'e' collate "en_US";
-select 'lock_note' collate "en_US" < 'locked_by' collate "en_US";
+SELECT
+    version();
+
+SELECT
+    '_' COLLATE "en_US" < 'e' COLLATE "en_US";
+
+SELECT
+    'lock_note' COLLATE "en_US" < 'locked_by' COLLATE "en_US";
+
+SELECT
+    'lock_note' COLLATE "C" < 'locked_by' COLLATE "C";
+
+SELECT
+    UNNEST(ARRAY['id' COLLATE "C", 'organization_id' COLLATE "C", 'locked_by' COLLATE "C", 'lock_reasons' COLLATE "C", 'lock_note' COLLATE "C", 'lock_source' COLLATE "C", 'sent_notification_emails' COLLATE "C", 'created_at' COLLATE "C"])
+ORDER BY
+    1;
