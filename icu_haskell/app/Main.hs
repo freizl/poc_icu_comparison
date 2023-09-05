022e3bdabd93d3d1145274f7d8f94281c4265e1d
Haisheng W - M <haisheng@mercury.com>
2023-09-05 12:23:29 -0700

    expand haskell sample

--- icu_haskell/app/Main.hs
+++ icu_haskell/app/Main.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Data.List (sort, sortBy)
 import Data.Text qualified as T (pack, Text)
 import Data.Text.ICU qualified as ICU
+import Text.Collate qualified as TC
 
 main :: IO ()
 main = do
@@ -18,9 +20,14 @@ main = do
             ] ::
                 [String]
     let ys = fmap T.pack xs
+    putStrLn "Original data: " >> putStr "\n"
     print xs
+    putStrLn "Default sort:" >> putStr "\t"
     print (sort ys)
+    putStrLn "Sort by text-icu:" >> putStr "\t"
     print (sortBy myCompare ys)
+    putStrLn "Sort by Text.Collate:" >> putStr "\t"
+    print (sortBy (TC.collate "en_US") ys)
 
 myCompare :: T.Text -> T.Text -> Ordering
 myCompare =
